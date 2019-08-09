cba51693d0457743a0f0b69c3914ae4331f97a29
tianma <tianma@paysupporter.com>
2019-08-09 18:46:25 +0800

    add

---
+++ src/main/java/com/designpattern/proxy/proxy/BasicProxy.java
@@ -0,0 +1,21 @@
+package com.designpattern.proxy.proxy;
+
+import java.io.File;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+public class BasicProxy {
+
+    static {
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+        new File("com/sun/proxy").mkdirs();
+    }
+
+    public static void main(String[] args) {
+        InvocationHandler handler = new DoraemonHandler(new KingRobot());
+        Clean proxy = (Clean) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[]{Clean.class}, handler);
+        System.out.println(String.format("代理者的类型为 %s", proxy.getClass().getName()));
+        System.out.println(proxy.hashCode());
+        proxy.work();
+    }
+}
