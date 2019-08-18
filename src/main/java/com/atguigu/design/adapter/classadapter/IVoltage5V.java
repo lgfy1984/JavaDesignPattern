b6c29f097409435f4d54c9c9163c4a488186394b
Administrator <lgfy1984@gmail.com>
2019-08-19 00:24:57 +0800

    add

--- src/main/java/com/atguigu/design/adapter/classadapter/IVoltage5V.java
+++ src/main/java/com/atguigu/design/adapter/classadapter/IVoltage5V.java
@@ -1,7 +1,7 @@
 package com.atguigu.design.adapter.classadapter;
 
 
-//����ӿ�
+//适配接口
 public interface IVoltage5V {
 	public int output5V();
 }
--- src/main/java/com/atguigu/design/adapter/classadapter/VoltageAdapter.java
+++ src/main/java/com/atguigu/design/adapter/classadapter/VoltageAdapter.java
@@ -1,14 +1,15 @@
 package com.atguigu.design.adapter.classadapter;
 
-//��������
+
+//适配器类
 public class VoltageAdapter extends Voltage220V implements IVoltage5V {
 
 	@Override
 	public int output5V() {
 		// TODO Auto-generated method stub
-		//��ȡ��220V��ѹ
+		//获取到220V电压
 		int srcV = output220V();
-		int dstV = srcV / 44 ; //ת�� 5v
+		int dstV = srcV / 44 ; //转成 5v
 		return dstV;
 	}
 
